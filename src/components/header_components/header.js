29a2982afbb07f21ab43a8a2b9dd1d9321acc72b
dajamante <aimaiga2@gmail.com>
2019-08-24 08:34:32 +0200

    unstyly header

--- src/components/header_components/header.js
+++ src/components/header_components/header.js
@@ -1,19 +1,26 @@
 import React from "react"
 import { Link } from "gatsby"
 import styles from "./header.module.css"
+import { FaDog } from "react-icons/fa"
+import { GoOctoface } from "react-icons/go"
+import { GiHealthNormal } from "react-icons/gi"
 
 const Header = () => (
   <header className={styles.header}>
     <div className={styles[`header__wrap`]}>
+      <GiHealthNormal className={styles[`main_icon`]} />
       <nav role="main" className={styles[`header__nav`]}>
-        <Link to="/" className={styles[`header__link`]}>
-          Home
-        </Link>
         <Link to="/app/profile" className={styles[`header__link`]}>
           Profile
         </Link>
+        <a
+          href="https://github.com/Dajamante/"
+          className={styles[`header__link`]}
+        >
+          <GoOctoface />
+        </a>
         <a href="/hello-world/" className={styles[`header__link`]}>
-          Details
+          About this blog
         </a>
       </nav>
     </div>
